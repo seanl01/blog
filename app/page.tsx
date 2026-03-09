d30d3bfd76af5f04f9c33d05ad5a1a3059113220
seanl01 <57535001+seanl01@users.noreply.github.com>
2026-03-08 21:51:22 -0400

    feat: update email

--- app/page.tsx
+++ app/page.tsx
@@ -33,6 +33,10 @@ export default function Page() {
         I'm currently a Masters of Information Systems Management at <a className="underline" href="https://cmu.edu" target="_blank" rel="noopener noreferrer">Carnegie Mellon University</a> (Graduating Fall '26), where I learn about managing complexity in IT and
         making informed decisions.
         In my free time, I enjoy fiddling with jazz on the piano, playing a game of <a className="underline" href="https://github.com/seanl01/football-trainer" target="_blank" rel="noopener noreferrer">football</a> (soccer) with friends, or drinking a good iced latte.
+
+        <br></br>
+        <br></br>
+        Reach me at <span className="font-medium">seanlim2<span className="hidden">antispam</span>@gmail<span className="hidden">antispam</span>.com</span>
       </p>
       <div className="my-8">
         <h2 className="mb-8 text-xl font-semibold">Featured Posts</h2>
